72b5d01a0d72ec312000b202f8c6f84bc4c5f1de
Fletcher Gibson <fletcher.gibson@students.mq.edu.au>
2018-09-30 23:12:44 +1000

    updates

--- aao_cutout_api/aao_cutout_api/settings/dev.py
+++ aao_cutout_api/aao_cutout_api/settings/dev.py
@@ -1,7 +1,6 @@
 from aao_cutout_api.settings.base import *
 
 #Override base.py settings here
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='apqga4h8g=n6x!oh-6976x4q%^+iadbqwj%p+$an0(j)8c2b^f') #os.environ['SECRET_KEY']
 
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
 
