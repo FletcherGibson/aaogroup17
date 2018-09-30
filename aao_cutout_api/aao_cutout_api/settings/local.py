f9b52f520db2e9222d6b46615abc42e201660c75
Fletcher Gibson <fletcher.gibson@students.mq.edu.au>
2018-09-30 23:41:48 +1000

    updates

--- aao_cutout_api/aao_cutout_api/settings/local.py
+++ aao_cutout_api/aao_cutout_api/settings/local.py
@@ -1,5 +1,5 @@
 from aao_cutout_api.settings.base import *
 
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='apqga4h8g=n6x!oh-6976x4q%^+iadbqwj%p+$an0(j)8c2b^f') #os.environ['SECRET_KEY']
+
 
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
