4d649ecc7f78c72e89d22af2ec2dc7b85733d7f1
Fletcher Gibson <fletcher.gibson@students.mq.edu.au>
2018-11-04 21:05:22 +1100

    more refactoring

--- remote_imaging_micro_service/remote_imaging_micro_service/fetch_options.py
+++ remote_imaging_micro_service/remote_imaging_micro_service/fetch_options.py
@@ -1,4 +1,20 @@
+from bs4 import BeautifulSoup as BS
+from urllib.request import urlopen
+
+
 def gleam_options():
-    # do something..
-    test_list = [1, 2, 3]
-    return test_list
+    gleam_page = 'http://gleam-vo.icrar.org/gleam_postage/q/form'
+
+    options_page = urlopen(gleam_page)
+
+    soup = BS(options_page, 'html.parser')
+
+    freq_options = soup.find('select', attrs={'name': 'freq'})
+
+    gleam_freq_options = ()
+
+    for option in freq_options:
+        choice = ('mwagleam_dr1_' + option.text, option.text)
+        gleam_freq_options + choice
+
+    return gleam_freq_options
